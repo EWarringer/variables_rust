7423b5da3d2a10793fd0b73dfb6f0947028e6bc0
Erik Warringer <erikwarringer@gmail.com>
2017-12-26 15:03:55 -0500

    another example

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,16 @@
+// fn main() {
+//     let mut x = 5;
+//     println!("The value of x is: {}", x);
+//     x = 6;
+//     println!("The value of x is: {}", x);
+// }
+
 fn main() {
-    let mut x = 5;
-    println!("The value of x is: {}", x);
-    x = 6;
-    println!("The value of x is: {}", x);
+    let c = 5;
+
+    let c = c + 1;
+    
+    let c = c * 2;
+    
+    println!("The value of c is: {}", c);
 }
